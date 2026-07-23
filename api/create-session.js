0fa4b1d2d02d5ed15d564740b446b4d44a34f2dc
rhrissayman0-crypto <rhrissayman0@gmail.com>
2026-07-23 20:25:15 +0100

    Update create-session.js

--- api/create-session.js
+++ api/create-session.js
@@ -1,9 +1,9 @@
 export default async function handler(req, res) {
-  if (req.method !== "POST") {
-    return res.status(405).json({
-      error: "Method not allowed"
-    });
-  }
+  if (req.method !== "GET") {
+  return res.status(405).json({
+    error: "Method not allowed"
+  });
+}
 
   try {
     const response = await fetch("https://verification.didit.me/v3/session/", {
